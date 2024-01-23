3f81b757cd9c54f2fbe625851bff5789d29169f4
Winnie YZ <fgyost@gmail.com>
2024-01-23 10:37:00 -0800

    initial upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>"Ineligible" Dave McCarty</title>
+    <style>
+        a{
+            color:#728FCE
+        }
+        a:visited{
+            color:#9E7BFF;
+        }
+    </style>
+</head>
+<body style='background-color:#101010;color:#cccccc;text-align:center;'>
+<div style='margin:3em;'>
+<?php
+$date1 = new DateTime("2024-01-20");
+$date2 = new DateTime();
+$interval = $date1->diff($date2);
+echo "<h2 style='background-color:#202020;border-radius:20px;padding:10px;'> It has been <span style='border:thin solid white; padding-left:3px;padding-right:3px;color:#B83C08;'>" . $interval->days . "</span> days since the 2023 Hugo Nomination Statistics have been released, and \"Ineligible\" Dave McCarty, through incompetence or maliciousness, has not given a clear reason why certain works/people were removed from the ballot as Ineligible</h2>";
+?>
+<div style='background-color:#202020;border-radius:20px;padding:10px;'>
+<p>He has released the meaningless statement "We looked at the rules and laws and then we decided".</p>
+<p>But he can't cite the rules or laws in question</p>
+<p>And he quickly sunk to lobbing insults when people didn't accept this</p>
+</div>
+<h3 style='background-color:#202020;border-radius:20px;padding:10px;'>As a fandom, let's keep this guy from holding any positions of power going forward, yeah?</h3>
+<div style='background-color:#202020;border-radius:20px;padding:10px;text-align:left;width:50%;margin:auto;'>
+<h4>Anyway, check these folks out and support them:</h4>
+<ul>
+<li>R. F. Kuang: <a href='https://www.bookpeople.com/book/9780063021433'>Babel</a></li>
+<li>Paul Weimer: <a href='https://www.patreon.com/princejvstin'>Patreon</a></li>
+<li>Xiran Jay Zhao: <a href='https://xiranjayzhao.com/'>xiranjayzhao.com</a></li>
+<li>Neil Gaiman: <a href='https://www.netflix.com/title/81150303?source=35'>Sandman Ep 6 (on netflix)</a></li>
+</ul>
+</div>
+<div style='background-color:#202020;border-radius:20px;padding:10px;margin-top:2em;'>
+<h3>That doesn't even begin to cover the data irregularities</h3>
+<div style='background-color:#202020;border-radius:20px;padding:10px;text-align:left;width:75%;margin:auto;'>
+<ul>
+<li>some serious irregularities with the numbers in the nomination data, most of note is how the EPH totals in Column 9 for four categories (Best Novel, Best Short Story, Best Fan Writer and Lodestar) exceed the number of ballots cast, which should be impossible. Several more categories, said total equals over 90% of the ballots cast, which is not impossible but highly improbable, implying almost every ballot nominated at least one of the final 15 on the longlist.</li>
+<li>in many categories, there is a wide difference between the number of nominations the finalists received compared to the next ones on the ranking. For example, in Best Series, we see six finalists that all received over 800 nominations— garnering more than double, if not triple what any nominee in this category had received in the history of the award-- and then the 7th receiving 52. This sort of distribution is too unlikely to be natural, it speaks of some form of tampering.</li>
+</ul>
+(Asked by Marshall Ryan Maresca on FB/<a href='https://bsky.app/profile/mrmaresca.com/post/3kjm5m2g2fn26'>Bluesky</a>)
+</div>
+</div>
+<h3 style='background-color:#202020;border-radius:20px;padding:10px;'>"Ineligible Dave" is catchier than "Refuses to Explain the Weird Stuff With The Numbers Dave" but both seem fitting thus far</h3>
+</div>
+</body>
+</html>
